9a4f48132bb30d17cdf86fe55cc9ae8025f451ab
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-07-02 20:29:58 -0600

    Responsive design using Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontSize:{
+      small:'0.1rem',
+      medium: '0.7rem',
+      large: '0.9rem'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
